9062546dedf81c999d2b0828ecf4d595557c5989
baskooijman <bas.kooijman@vu.nl>
2020-02-11 09:00:00 +0100

    get dates of saved  AmPdata

---
+++ date_allStat.m
@@ -0,0 +1,21 @@
+%% date_allStat
+% get date of allStat.mat file
+
+%
+function date = date_allStat
+% created at 2020/02/11 by Bas Kooijman
+
+  %% Syntax
+  % date = <date_allStat.m *date_allStat*>
+
+  %% Description
+  % Get date of allStat.mat
+  %
+  % Output
+  %
+  % * date string
+
+  %% remark
+  % convert to yyyy/mm/dd with datestr(datenum(date_allStat), 'yyyy/mm/dd')
+  
+  allStatInfo = dir(which('allStat.mat')); date = allStatInfo.date; 
---
+++ date_popStat.m
@@ -0,0 +1,21 @@
+%% date_popStat
+% get date of popStat.mat file
+
+%
+function date = date_popStat
+% created at 2020/02/11 by Bas Kooijman
+
+  %% Syntax
+  % date = <date_popStat.m *date_popStat*>
+
+  %% Description
+  % Get date of popStat.mat
+  %
+  % Output
+  %
+  % * date string
+
+  %% remark
+  % convert to yyyy/mm/dd with datestr(datenum(date_popStat), 'yyyy/mm/dd')
+  
+  popStatInfo = dir(which('popStat.mat')); date = popStatInfo.date; 
