741cb76bbe8dd306702b704c480628236acc6697
baskooijman <bas.kooijman@vu.nl>
2021-05-13 16:32:29 +0200

    Update mydata_mds_Carnivora.m

--- mydata_mds_Carnivora.m
+++ mydata_mds_Carnivora.m
@@ -1,5 +1,7 @@
-% example of how to use dist_traits in classic multidimensional scaling, using 3 dimensions
-% click on rotation in toolbar of figure to rotate figure by grabbing it
+%% example of how to use dist_traits in classic multidimensional scaling, using 3 dimensions
+% 2021/05/13 by Bas Kooijman
+% Click on rotation in toolbar of figure to rotate figure by grabbing it
+% If you did not yet run read_stat this Matlab session, the first time might take a while; next trial are faster.
 
 species = select('Carnivora');
 traits = {'a_m'; 'a_p'; 'a_b'; 'Ww_i'; 'Ww_p'; 'Ww_b'; 'R_i'; 's_s'; 's_Hbp'; 'p_M'};
