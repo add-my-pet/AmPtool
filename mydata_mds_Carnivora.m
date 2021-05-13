4accf609d59fb4a089823c11080dea179a82c43d
baskooijman <bas.kooijman@vu.nl>
2021-05-13 21:23:36 +0200

    Update mydata_mds_Carnivora.m

--- mydata_mds_Carnivora.m
+++ mydata_mds_Carnivora.m
@@ -1,10 +1,16 @@
 %% example of how to use dist_traits in classic multidimensional scaling, using 3 dimensions
 % 2021/05/13 by Bas Kooijman
+% Click on points to see species, but only before you click on rotation
 % Click on rotation in toolbar of figure to rotate figure by grabbing it
-% If you did not yet run read_stat this Matlab session, the first time might take a while; next trial are faster.
+% If you did not yet run read_stat this Matlab session, the first time you run this script might take a while; next trials are faster.
+
+close all
 
 species = select('Carnivora');
+% traits = {'a_m'; 'a_p'; 'a_b'; 'Ww_i'; 'Ww_p'; 'Ww_b'; 'R_i'; 's_s'; 's_Hbp'; 'p_M'; 'v'; 'kap'; 'E_Hb'; 'E_Hx'; 'E_Hp'};
 traits = {'a_m'; 'a_p'; 'a_b'; 'Ww_i'; 'Ww_p'; 'Ww_b'; 'R_i'; 's_s'; 's_Hbp'; 'p_M'};
+% traits = {'Ww_i'; 'Ww_p'; 'Ww_b'; 'R_i'; 's_s'; 's_Hbp'};
+
 % first compute distance-matrix and pass it to cmdscale
 y = cmdscale(dist_traits(species, traits),3);
 
@@ -21,7 +27,7 @@ y = cmdscale(dist_traits(species, traits),3);
   };
 
 % make sure that the number of rows of data matches the number of entries
-data=NaN(length(select),3); data(select_01('Carnivora'),:) = y;
+data = NaN(length(select),3); data(select_01('Carnivora'),:) = y;
 
 % plot with legend in second figure
 shstat(data, legend_Carnivora, 'MDS for Carnivora');
