6cc30ed89327f9cfd37decab37c47f14fc891e44
baskooijman <bas.kooijman@vu.nl>
2021-07-10 10:44:29 +0200

    Update mydata_mds.m

--- mydata_mds.m
+++ mydata_mds.m
@@ -8,19 +8,20 @@
 
 close all
 
-species = select;
+species = select('Chondrichthyes');
 % traits = {'a_m'; 'a_p'; 'a_b'; 'Ww_i'; 'Ww_p'; 'Ww_b'; 'R_i'; 's_s'; 's_Hbp'; 'p_M'; 'v'; 'kap'; 'E_Hb'; 'E_Hp'};
 traits = {'a_m'; 'a_p'; 'a_b'; 'Ww_i'; 'Ww_p'; 'Ww_b'; 'R_i'; 's_s'; 's_Hbp'; 'p_M'};
 % traits = {'Ww_i'; 'Ww_p'; 'Ww_b'; 'R_i'; 's_s'; 's_Hbp'};
 
 % first compute distance-matrix and pass it to cmdscale
-[y, e] = cmdscale(dist_traits(species, traits)); % configuration matrix, eigenvalues
+distances = dist_traits(species, traits);
+[y, e] = cmdscale(distances); % configuration matrix, eigenvalues
 
 % make sure that the number of rows of data matches the number of entries
-% data = NaN(length(select),3); data(select_01('Animalia'),:) = y(:,1:3);
+data = NaN(length(select),3); data(select_01('Chondrichthyes'),:) = y(:,1:3);
 
 % plot with legend in second figure
-Hfig = shstat(data, legend_RSED, 'MDS for Animalia');
+Hfig = shstat(data, legend_shark, 'MDS for Chondrichthyes');
 
 % connect the points for subclades
 % connect_subclade(Hfig, y(:,1:3), 'Animalia', 'Chelonioidea');
@@ -30,3 +31,6 @@ n_e = length(e); n_t = length(traits);
 plot(1:n_t, e(1:n_t), '*b', n_t+1:n_e, e(n_t+1:n_e), '*r')
 xlabel('rank');
 ylabel('eigenvalue');
+
+PhyloTree = seqlinkage(distances, 'single', species);
+view(PhyloTree);
