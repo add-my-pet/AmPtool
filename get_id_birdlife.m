18a8ec2b4f9a984bfa66ce48c7ddb503dff06db0
Bas Kooijman <bas.kooijman@vu.nl>
2021-08-02 11:15:27 +0200

    extension

--- get_id_birdlife.m
+++ get_id_birdlife.m
@@ -24,7 +24,7 @@ function [id_birdlife id_avibase] = get_id_birdlife(my_pet, open)
 %% Remarks
 % Outputs empty strings if identification was not successful.
 % Does not work perfectly, since id is derive from twitter name in avibase.
-% Possible problems: corn-crake, rather than corncrake, hauxwell's-thrush, rather than hauxwells-thrush
+% Possible problems e.g. corn-crake, rather than corncrake
 
 %% Example of use
 % [id_birdlife id_avibase] = get_id_birdlife('Passer_domesticus', 1)
@@ -37,10 +37,20 @@ if ~exist('open','var')
 end
 
 id_avibase = get_id_avibase(my_pet); if isempty(id_avibase); id_birdlife = []; return; end
-url = urlread(['https://avibase.bsc-eoc.org/species.jsp?lang=EN&avibaseid=', id_avibase]);
-i_0 = strfind(url,'name="twitter:title" content="') + 30;
-i_1 = strfind(url(i_0:end), '"') + i_0(1) - 2;
-id = lower(url(i_0(1): i_1(1))); id = strrep(id, '- ', '-'); id_birdlife = strrep(id, ' ', '-');
+% url = urlread(['https://avibase.bsc-eoc.org/species.jsp?lang=EN&avibaseid=', id_avibase]);
+% i_0 = strfind(url,'name="twitter:title" content="') + 30;
+% i_1 = strfind(url(i_0:end), '"') + i_0(1) - 2;
+% id = lower(url(i_0(1): i_1(1))); 
+% id = strrep(id, '- ', '-'); id = strrep(id, ' ', '-'); id = strrep(id, '''', '');
+% 
+% id_birdlife = id;
+
+my_pet = strrep(my_pet,'_','+'); 
+my_pet = strrep(my_pet,' ','+'); 
+
+url = urlread(['http://datazone.birdlife.org/quicksearch?qs=', my_pet]);
+i_0 = strfind(url,'factsheet/') + 10; i_1 = strfind(url(i_0(1):end),'"') - 2;
+id_birdlife = url(i_0(1): i_1(1));
 
 if open
   web([address, id_birdlife],'-browser');
