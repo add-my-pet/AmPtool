c1e0ec73785871b227441162d3098e94b7eef4e0
Bas Kooijman <bas.kooijman@vu.nl>
2021-09-17 11:57:19 +0200

    extension

--- get_id.m
+++ get_id.m
@@ -196,7 +196,7 @@ function [id, id_txt, my_pet_acc] = get_id(my_pet, open, tab)
     id{22} = get_id_MSW3(my_pet_acc); id_txt{22} = 'id_msw3';
     if isempty(id{22}) && ~strcmp(my_pet,my_pet_acc); id{22} = get_id_MSW3(my_pet); end
   end
-  if ismember(lin(ismember(rank,'Class')), {'Amphibia''Reptilia','Squamata','Testudines','Crocodilia','Aves','Mammalia'}) 
+  if ismember(lin(ismember(rank,'Class')), {'Amphibia','Reptilia','Squamata','Testudines','Crocodilia','Aves','Mammalia'}) 
     select_id(23) = true;
     id{23} = get_id_AnAge(my_pet_acc); id_txt{23} = 'id_AnAge';
     if isempty(id{23})  && ~strcmp(my_pet,my_pet_acc); id{23} = get_id_AnAge(my_pet); end
