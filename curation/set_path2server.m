ded015c7c7424d2517e5539417cb070df0c34bfd
Bas Kooijman <bas.kooijman@vu.nl>
2022-01-29 13:17:59 +0100

    Create set_path2server.m

---
+++ curation/set_path2server.m
@@ -0,0 +1,21 @@
+%% set_path2server
+% set path for browser to server
+
+function path2server = set_path2server
+% created 2021/02/25 by Bas Kooijman
+
+%% Syntax
+% path2server = <../set_path2server.m *set_path2server*>
+
+%% Description
+% fill a global with the name of the path to the deblab folder on the server
+%
+% Output
+%
+% * path2server: character string with paht to server
+
+path2server = 'https://www.bio.vu.nl/thb/deb/deblab/';
+<<<<<<< HEAD
+% path2server = 'https://debtheory.fr/';
+=======
+>>>>>>> 0534ffe6da9d7d468268d4065d6d4ed657b485de
