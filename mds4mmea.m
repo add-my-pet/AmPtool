2c0d0eef740aa7fdbfd09cc63d93ca573a5516f5
Bas Kooijman <bas.kooijman@vu.nl>
2022-04-01 09:34:54 +0200

    mds4mmea

--- mds4mmea.m
+++ mds4mmea.m
@@ -25,17 +25,15 @@ function [y, e, parNm, dist] = mds4mmea(results)
   % * dist: (n,n) distance matrix
   
   %% Example of use
-  % [y, e, parNms] = mds4mmea('results_Dipodomys_deserti');
+  % [y, e, parNms] = mds4mmea('results_Dipodomys_deserti_mmea');
   
   global lossfuction
   
-  close all
-
   lossfunction = 'sb';
   
   load(results)
-  data = result.solutionsParameters; % (n,k) matrix with (solutions,parameters) 
-  [n_data, n_par] = size(data);
+  sol = result.solutionsParameters; % (n,k) matrix with (solutions,parameters) 
+  [n_sol, n_par] = size(sol);
   parNm = result.parameterNames; % cell-string with names of free parameters
   if length(parNm) ~= n_par
     fprintf('Warning from mds4mmea: parameter names are not consistent with solution set\n');
@@ -45,19 +43,19 @@ function [y, e, parNm, dist] = mds4mmea(results)
   % compose legend: lowest loss function value with a light-red square, highest in black
   val = result.lossFunctionValues; % loss function values
   val_max = max(val); [val_min, i_min] = min(val);
-  valColor = color_lava(.95*(val_max - val)/ (val_max - val_min)); % rgb colors for data points
-  legend = cell(n_data,2); 
-  for i=1:n_data
+  valColor = color_lava(.95*(val_max - val)/ (val_max - val_min)); % rgb colors for sol points
+  legend = cell(n_sol,2); 
+  for i=1:n_sol
     legend{i,1} = {'o', 6, 3, valColor(i,:), valColor(i,:)}; 
     legend{i,2} = num2str(i); 
   end
   legend{i_min,1}{1} = 's'; % replace circle by square
           
   % distance matrix & mds
-  dist = zeros(n_data, n_data); % initiate distance matrix
-  for i = 1:n_data
-    for j = i+1:n_data
-      dist(i,j) = sum((data(i,:) - data(j,:)).^2 ./ (data(i,:).^2 + data(j,:).^2));
+  dist = zeros(n_sol, n_sol); % initiate distance matrix
+  for i = 1:n_sol
+    for j = i+1:n_sol
+      dist(i,j) = sum((sol(i,:) - sol(j,:)).^2 ./ (sol(i,:).^2 + sol(j,:).^2));
     end
   end
   dist = dist + dist'; % make distance matix symmetric
@@ -72,10 +70,10 @@ function [y, e, parNm, dist] = mds4mmea(results)
   box on
 
   % print correlations between parameters and first 3 eigenvectors 
-  prt_tab({parNm,corr(data,y(:,1:3))},{'par', 'axis 1', 'axis 2', 'axis 3'}, 'classical mds');
+  prt_tab({parNm,corr(sol,y(:,1:3))},{'par', 'axis 1', 'axis 2', 'axis 3'}, 'classical mds');
 
   figure % plot eigenvalues
-  plot(1:n_par, e(1:n_par), '*b', n_par+1:n_data, e(n_par+1:n_data), '*r')
+  plot(1:n_par, e(1:n_par), '*b', n_par+1:n_sol, e(n_par+1:n_sol), '*r')
   xlabel('rank');
   ylabel('eigenvalue');
   title([num2str(n_par), ' parameters estimated'])
