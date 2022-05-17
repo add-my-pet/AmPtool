e589da568820daa422a0e6ab7d047921626a8638
Bas Kooijman <bas.kooijman@vu.nl>
2022-05-17 09:17:33 +0200

    Update run_collection_intro.m

--- curation/run_collection_intro.m
+++ curation/run_collection_intro.m
@@ -86,6 +86,7 @@ prt_pars; % add_my_pet/pars.html
 prt_patterns; % add_my_pet/patterns.html
 prt_pie_SGGJR; % add_my_pet/pie_pSGJRb.html, pie_pSGJRi.html, pie_pSGJRp.html, pie_SGJRb.html
 prt_about; % add_my_pet/about.html
+prt_id(select,1); % id of all links to html pages
 
 % cleanup
 delete('..\taxa\*.txt~','..\taxa\*.txt#') % delete emacs backup-files
