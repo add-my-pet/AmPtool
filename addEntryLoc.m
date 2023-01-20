3e04f48990825b0fa44e18a6396fd97e15556bc1
Bas Kooijman <bas.kooijman@vu.nl>
2023-01-20 13:40:52 +0100

    addEntryLoc

--- addEntryLoc.m
+++ addEntryLoc.m
@@ -22,7 +22,9 @@ function addEntryLoc(my_pet)
 %% Remarks
 % This function assumes that the file results_my_pet.mat exists in the
 % local directory, where my_pet is replaced with the name of your entry.
-% It also assumes that your entry is already added to the lists-of-lists.
+% It also assumes that the species is already added to the lists-of-lists.
+% This function does not change AmPdata.zip, allUnits, allLabel 
+% Warning: this function uses clear all to replace AmPdata/allStat.mat, popStat.mat and AmPtool/n_entries.mat.
 
 %% Example of use
 % addEntryLoc('my_pet'), where my_pet is replaced with the name of your entry.
@@ -40,20 +42,30 @@ catch
   return
 end
 
-return
+WD = pwd;
 
-WD = cdCur; % go to AmPtool/curation
+cdCur; % go to AmPtool/curation
+% write number of entries in lists-of-lists in Amtool/taxa/n_taxa.mat; this avoids the need to run length(select)
+n_entries = length(select); WD = cdCur; cd ../taxa; save('n_entries.mat', 'n_entries'); cd(WD);
 
-if ~exist('entries','var')
-  entries = select;
+% go to AmPdata and check if my_pet is already in allStat
+cdAmPdata; clear all; load allStat.mat % load allUnits allLabel;
+if isfield(allStat, my_pet)
+  fprintf('Warning from addEntryLoc: %s already exists allStat.mat\n', my_pet);
+  return
 end
-
-% allStat and popStat are made persistent and must be overwritten
-clear all; load('temporary.mat'); 
-
 % write add_my_pet/AmPdata/allStat.mat and popStat.mat
 [allStat, info] = write_addStat(my_pet); % this adds/modifies allStat for selected entries
-if ~info; return; end
-write_popStat_loc(my_pet); % collects entries_web/my_pet_pop.mat files in structure popStat
+if ~info 
+   fprintf('Warning from addEntryLoc: addition to allStat.mat was unsuccessful\n');
+   return
+end
+
+% prepare adding to popStat
+load popStat
+[stat, Hfig_surv, Hfig_stab] = popStatistics_st(model, par, T, f);
+popStat.(my_pet) = stat; popStat.(my_pet).T = T; 
+popStat.(my_pe).model = model; popStat.(my_pet).par = par; 
+save('popStat.mat','popStat'); 
 
 cd(WD); % return to original directory
