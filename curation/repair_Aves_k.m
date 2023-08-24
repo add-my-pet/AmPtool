6e54eb65530977f1c7120df9bdcc3b95b79fc775
Starrlight Augustine <starraugustine@users.noreply.github.com>
2023-08-24 07:44:22 +0100

    Update repair_Aves_k.m

--- curation/repair_Aves_k.m
+++ curation/repair_Aves_k.m
@@ -35,9 +35,9 @@ function repair_Aves_k(entries)
 %% Example
 % nm=select('Aves'); repair_Aves_k(nm); 
 % continue estimation with "estim_pars; mat2pars_init" move to the next species with "dbcont" 
-% After quitting at entry i, continue with nm(1:i-1)=[]; repair_Aves_k(nm)
+% After quitting at entry i, continue with nm=select('Aves');nm(1:i-1)=[]; repair_Aves_k(nm)
 % If i is lost. but entry nm_i is in the the Matlab editor, recover i by 
-% nm=select('Aves'); ind=1:length(nm); i = ind(strcmp(nm_i,nm)); nm(1:i-1)=[]; 
+% nm=select('Aves'); ind=1:length(nm); i = ind(strcmp(nm_i,nm)); nm(1:i-1)=[]; repair_Aves_k(nm); 
 
 WD = cdCur; cd '../../deblab/add_my_pet/Aves_k/Struthio_camelus'; % first bird
  
@@ -144,15 +144,15 @@ for i=1:n % scan entries
       ind_0 = 13 + strfind(mydata, 'temp.ab = C2K('); 
       ind_1 = ind_0(1) - 1 + strfind(mydata(ind_0(1):end),')'); 
       mydata = [mydata(1:ind_0(1)), '36', mydata(ind_1(1):end)]; 
-      if contains(mydata, 'comment.ab')
-        ind = strfind(mydata, 'comment.ab'); ind_0 = strfind(mydata(ind:end),''''); ind = ind + ind_0(1);
-        mydata = [mydata(1:ind), 'Temperature is guessed based on being the optimal temperature for artificial incubation for several species; ' , ...
-         'It depends on environmental conditions and parental care. ', mydata(ind+1:end)];   
-      else
-        ind = -1+strfind(mydata, 'data.tx');
-        mydata = [mydata(1:ind), '  comment.ab = ''temperature is guessed based on being the optimal temperature for artificial incubation for several species; ' , ...
-         'It depends on environmental conditions and parental care'';', char(13), mydata(ind+1:end)];
-      end
+%       if contains(mydata, 'comment.ab')
+%         ind = strfind(mydata, 'comment.ab'); ind_0 = strfind(mydata(ind:end),''''); ind = ind + ind_0(1);
+%         mydata = [mydata(1:ind), 'Temperature is guessed based on being the optimal temperature for artificial incubation for several species; ' , ...
+%          'It depends on environmental conditions and parental care. ', mydata(ind+1:end)];   
+%       else
+%         ind = -1+strfind(mydata, 'data.tx');
+%         mydata = [mydata(1:ind), '  comment.ab = ''temperature is guessed based on being the optimal temperature for artificial incubation for several species; ' , ...
+%          'It depends on environmental conditions and parental care'';', char(13), mydata(ind+1:end)];
+%       end
 %     else
 %       fprintf('Warning from repair_Aves_k: mydata contains the word embryo, please edit temperatures by hand\n') 
 %       fprintf('The typical body temperature for this order is %s C\n', T_typical) 
@@ -164,18 +164,13 @@ for i=1:n % scan entries
 
   %% edit pars_init
     
- % set initial estimate for v
-  pars_init = strrep(pars_init, 'par.v = ', 'par.v = 0.02;');
-  pars_init = strrep(pars_init, 'free.kap   = 1;', 'free.kap   = 0;');
-
-  
-  % free v
+%   % free v
   ind_0 = 8 + strfind(pars_init, 'free.v'); ind_1 =  strfind(pars_init(ind_0:end), ';'); ind_1 = ind_0 + ind_1(1) - 2;
   pars_init = [pars_init(1:ind_0), ' = 1;' , pars_init(ind_1:end)];
 
   % set initial estimate for k_J
 %   k_J = num2str(0.3 * par.p_M/ par.E_G); % 1/d, based on k = 0.3
-k_J = num2str(0.02); % 1/d, based on experience
+  k_J = num2str(0.02); % 1/d, based on experience
   ind_0 = 9 + strfind(pars_init, 'par.k_J = '); ind_1 =  strfind(pars_init(ind_0:end), ';'); ind_1 = ind_0 + ind_1(1) - 2;
   pars_init = [pars_init(1:ind_0), k_J, ';', pars_init(ind_1:end)];
 
@@ -185,8 +180,8 @@ k_J = num2str(0.02); % 1/d, based on experience
   pars_init = [pars_init(1:ind_0), ' = 0;' , pars_init(ind_1:end)];
   
   % reduce initial estimate for kap
-%   pars_init = strrep(pars_init, 'par.kap = ', 'par.kap = 0.9*');
-  pars_init = strrep(pars_init, 'par.kap = ', 'par.kap = 0.7;');
+% %   pars_init = strrep(pars_init, 'par.kap = ', 'par.kap = 0.9*');
+%   pars_init = strrep(pars_init, 'par.kap = ', 'par.kap = 0.7;');
   
 %   % fix kap
 %   ind_0 = 8 + strfind(pars_init, 'free.kap'); ind_1 =  strfind(pars_init(ind_0:end), ';'); ind_1 = ind_0 + ind_1(1) - 2;
@@ -197,18 +192,36 @@ k_J = num2str(0.02); % 1/d, based on experience
   t_R = num2str(data.tR);
   ind_0 = 9 + strfind(pars_init, 'par.t_R = '); ind_1 = strfind(pars_init(ind_0:end), ';'); ind_1 = ind_0 + ind_1(2);
   pars_init = [pars_init(1:ind_0), t_R, '; free.t_R = 0;' , pars_init(ind_1:end)];
-
+     
   % modify E_Hp, add E_Hx
   pars_init = strrep(pars_init, 'fledging/puberty', 'puberty');
   ind_0 = strfind(pars_init, 'par.E_Hp'); ind_1 = strfind(pars_init, 'par.h_a');
   txt_EHx = pars_init(ind_0:ind_1-1); txt_EHx = strrep(txt_EHx, 'E_Hp', 'E_Hx');
   txt_EHx = strrep(txt_EHx, 'puberty', 'fledging');
-  ind = ind_0 + 1 + strfind(pars_init(ind_0:end), ' = '); pars_init = [pars_init(1:ind), '2*', pars_init(ind+1:end)];
-  %
+%   ind = ind_0 + 1 + strfind(pars_init(ind_0:end), ' = '); pars_init = [pars_init(1:ind), '2*', pars_init(ind+1:end)];
+
+  % other parameters 
   ind = 21 + strfind(pars_init, '%% other parameters');
   pars_init  = [pars_init(1:ind), txt_EHx, pars_init(ind+1:end)];
   pars_init = strrep(pars_init, 'par.t_0', '% par.t_0');  
 
+  % starting values for birds
+  pars_init = strrep(pars_init, 'par.v = ', 'par.v = 0.02;');
+  pars_init = strrep(pars_init, 'par.kap = ', 'par.kap = 0.7;');
+  pars_init = strrep(pars_init, 'par.p_M = ', 'par.p_M = 500;');
+  pars_init = strrep(pars_init, 'par.E_Hb = ', 'par.E_Hb = 9.057e+02;');
+  pars_init = strrep(pars_init, 'par.E_Hx = ', 'par.E_Hx = 9.057e+02*2;');
+  pars_init = strrep(pars_init, 'par.E_Hp = ', 'par.E_Hp =   1.689e+03;');
+  pars_init = strrep(pars_init, 'par.h_a = ', 'par.h_a = 5.236e-12;');
+  pars_init = strrep(pars_init, 'par.z = ', 'par.z = 1.65;');
+  
+  % free or fix parameter values
+  % set initial estimate for v
+%   pars_init = strrep(pars_init, 'free.v     = 0;', 'free.v     = 1;');
+  pars_init = strrep(pars_init, 'free.kap   = 1;', 'free.kap   = 0;');
+  pars_init = strrep(pars_init, 'free.p_M   = 0;', 'free.p_M   = 1;');
+  pars_init = strrep(pars_init, 'free.E_Hp  = 0;', 'free.E_Hp  = 1;');
+  
   %% edit predict
   
   % TC (some entries do not use TC_ab)
@@ -221,6 +234,8 @@ k_J = num2str(0.02); % 1/d, based on experience
 %   ind_0 = -1 + strfind(predict, 'aT_b'); ind_1 = strfind(predict, '% d,'); 
 %   predict = [predict(1:ind_0), 'aT_b = t_0 + t_b/ k_M/ TC_ab;      ', predict(ind_1(1):end)];
 predict = strrep(predict, 'aT_b = t_0 + t_b/ k_M/ TC_ab;', 'aT_b = tau_b/ k_M/ TC_ab;');
+predict = strrep(predict, 'aT_b = t_0 + t_b/ k_M/ TC;', 'aT_b = tau_b/ k_M/ TC_ab;');
+predict = strrep(predict, 'aT_b = t_0 + t_b/ kT_M;', 'aT_b = tau_b/ k_M/ TC_ab;');
 predict = strrep(predict, 'aT_b = t_0 + tT_b;', 'aT_b = tT_b;');
 predict = strrep(predict, 'tT_b = t_b/ k_M/ TC_ab;', 'tT_b = tau_b/ k_M/ TC_ab;');
 predict = strrep(predict, 'if t_0 < 0', 'if E_Hx < 0');
