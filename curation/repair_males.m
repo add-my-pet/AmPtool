46e931dc2e8ff17e07eea0e81f899844f774efab
Bas Kooijman <bas.kooijman@vu.nl>
2023-09-04 08:06:09 +0200

    extension

--- curation/repair_males.m
+++ curation/repair_males.m
@@ -31,8 +31,17 @@ function nm = repair_males(entries)
 % nm_m = nm(s_L|s_Ww|s_W|s_Wb|s_Lb);
 % repair_males(nm_m)
 
+global pets
+
 cur2ez = '../../entries_zip/'; % path from AmPtool/curation to entries_zip
 cur2ea = '../../entries_archive/'; % path from AmPtool/curation to entries_archive
+
+estim_options('default'); 
+estim_options('max_step_number', 5e2); 
+estim_options('max_fun_evals', 5e3);
+
+estim_options('pars_init_method', 2); 
+estim_options('results_output', 3); 
 
 WD = cdCur; nm = cell(0,1);
     
@@ -47,26 +56,32 @@ for i=1:n % scan entries
   my_pet = entries{i}; 
   fprintf('%g: %s\n', i, my_pet);
   cdEntr(my_pet,0); % cd to add_my_pet/entries/my_pet and load source files
+  pets = {my_pet}; 
   
-  % get name of existing zip-file
-  list=cellstr(ls('../../entries_zip')); 
-  zipFnm = [cur2ez, list{~cellfun(@isempty,strfind(list,my_pet))}];
+  estim_options('method', 'nm'); 
+  %estim_options('pars_init_method', 1); 
+  estim_pars; 
+  mat2pars_init
+
+%   % get name of existing zip-file
+%   list=cellstr(ls('../../entries_zip')); 
+%   zipFnm = [cur2ez, list{~cellfun(@isempty,strfind(list,my_pet))}];
 
   fprintf('type dbcont to proceed or dbquit \n'); 
   keyboard
-  x = input('type 0 to skip move zip and run_collection ');
-  if ~(x==0)
+%   x = input('type 0 to skip move zip and run_collection ');
+%   if ~(x==0)
     % metaData: add mod and date_acc
     % add comment for mod
     % edit: predit_my pet 
     % re-estimate: run_my_pet
   
     % move zip from entries_zip to entries_archive
-    movefile(zipFnm,cur2ea)
+%     movefile(zipFnm,cur2ea)
     run_collection(my_pet)
     
-    nm = [nm; my_pet]; % append modified entry name
-  end
+%    nm = [nm; my_pet]; % append modified entry name
+%  end
   
   % close source file in editor
   editorTabs = matlab.desktop.editor.getAll;
