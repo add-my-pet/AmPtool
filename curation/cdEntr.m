cb4e62108cf9e23d64643226710bbfe7e8323dd1
Bas Kooijman <bas.kooijman@vu.nl>
2024-01-06 18:20:28 +0100

    extension

--- curation/cdEntr.m
+++ curation/cdEntr.m
@@ -1,8 +1,8 @@
 %% cdEntr
 % cd to the add_my_pet/entries/my_pet directory
 
-function WD = cdEntr(my_pet, ed, nm)
-% created 2021/02/22 by Bas Kooijman
+function WD = cdEntr(my_pet, ed)
+% created 2021/02/22 by Bas Kooijman, modified 2024/01/06
 
 %% Syntax
 % WD = <../cdEntr.m *cdEntr*> (my_pet, ed)
@@ -13,7 +13,6 @@ function WD = cdEntr(my_pet, ed, nm)
 %
 % * my_pet: string with name of entry
 % * ed: optional identifyer for editing in Matlab: 1 for mydata-file only, else all 4 source files
-% * nm: optional cell-string of names to which my_pet is appended.
 %
 % Output
 %
@@ -22,31 +21,31 @@ function WD = cdEntr(my_pet, ed, nm)
 %% Remarks
 % Intended use: 
 % * WD = cdEntr('Daphnia_magna',0); ..code.. cd(WD)
-% * nm={}; nm = cdEntr('my_pet1',1,nm); edit mydata_mypet1; nm = cdEntr('my_pet2',1,nm); edit mydata_mypet2; ...
-% * run_collection(nm); run_collection_intro(nm);
 
 if ~contains(my_pet,'_')
-  my_pet = strrep(my_pet,' ','_');
-end
-if exist('nm','var')
-  WD = [nm; my_pet];
-%   try 
-%     get_id_bow(my_pet,1);
-%   catch
-%     fprintf('warning from cdEntr: not found in bow\n');
-%   end
-else
-  WD = pwd; 
-end
-cdCur; cd(['../../deblab/add_my_pet/entries/', my_pet])
-
-if exist('ed', 'var')
-  if ed==1
-    edit(['mydata_', my_pet, '.m'])
+  if contains(my_pet,' ')
+    my_pet = strrep(my_pet,' ','_'); n = 1; nms = {my_pet};
   else
-    edit(['mydata_', my_pet, '.m'])
-    edit(['pars_init_', my_pet, '.m'])
-    edit(['predict_', my_pet, '.m'])
-    edit(['run_', my_pet, '.m'])
+    nms = select(my_pet); n = length(my_pet);
+  end
+else
+  nms = {my_pet}; n = 1;
+end
+
+WD = pwd;
+
+for i=1:n
+  my_pet = nms{i};
+  cdCur; cd(['../../deblab/add_my_pet/entries/', my_pet])
+
+  if exist('ed', 'var')
+    if ed==1
+      edit(['mydata_', my_pet, '.m'])
+    else
+      edit(['mydata_', my_pet, '.m'])
+      edit(['pars_init_', my_pet, '.m'])
+      edit(['predict_', my_pet, '.m'])
+      edit(['run_', my_pet, '.m'])
+    end
   end
 end
