8063f9863c4d0f49112d5be661b4bf69e34ceafc
Bas Kooijman <bas.kooijman@vu.nl>
2024-08-28 10:13:58 +0200

    cdEntr

--- curation/cdEntr.m
+++ curation/cdEntr.m
@@ -1,5 +1,5 @@
 %% cdEntr
-% cd to the add_my_pet/entries/my_pet directory
+% cd to the add_my_pet/entries/my_pet directory abd optional load files in editor
 
 function WD = cdEntr(my_pet, ed)
 % created 2021/02/22 by Bas Kooijman, modified 2024/01/06
@@ -9,8 +9,9 @@ function WD = cdEntr(my_pet, ed)
 
 %% Description
 % cd to the add_my_pet/entries/my_pet directory and open source files in Matlab editor
-% if nm exists, birdsoftheworld will be opened
 %
+% Input
+% 
 % * my_pet: string or cellstring with name(s) of entry
 % * ed: optional identifyer for editing in Matlab: 1 for mydata-file only, else all 4 source files
 %
@@ -20,21 +21,22 @@ function WD = cdEntr(my_pet, ed)
 
 %% Remarks
 % Intended use: 
-% * cdEntr('Daphnia_magna',0) or cdEntr('Daphnia',1) or cdEntr(select('Daphnia'),1)
+% * cdEntr('Daphnia_magna');
+% * WD = cdEntr('Daphnia_magna',0); cd(WD); 
+% * cdEntr('Daphnia',1) 
+% * cdEntr(select('Daphnia'),1)
 
 if iscell(my_pet)
   nms = my_pet; n = length(nms);  
 elseif contains(my_pet,'_')
   nms = {my_pet}; n = 1; 
+elseif contains(my_pet,' ')
+  my_pet = strrep(my_pet,' ','_'); nms = {my_pet}; n = 1;
 else
-  if contains(my_pet,' ')
-    my_pet = strrep(my_pet,' ','_'); n = 1; nms = {my_pet};
-  else
-    nms = select(my_pet); n = length(nms);
-  end
+  nms = select(my_pet); n = length(nms);
 end
 
-WD = pwd;
+WD = pwd; % copy to output
 
 for i=1:n
   my_pet = nms{i};
