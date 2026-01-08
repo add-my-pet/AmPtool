5ab9e8c646ba5ae67f10e8299ffe8201014cd38c
Bas Kooijman <bas.kooijman@vu.nl>
2026-01-08 10:14:51 +0100

    extension

--- legend_amphibia.m
+++ legend_amphibia.m
@@ -18,12 +18,12 @@ function legend = legend_amphibia
   % type, size, linewidth, edge color and face color of a marker, taxon
   legend = {...
     {'o', 8, 3, [0 0 0], [0 0 0]}, 'Gymnophiona'
-    {'o', 8, 3, [0 0 1], [0 0 0]}, 'Cryptobranchoidea' % Caudata
-    {'o', 8, 3, [0 0 1], [0 0 1]}, 'Salamandroidea'
-    {'o', 8, 3, [0 0 1], [1 0 0]}, 'Sirenoidea'
+    {'o', 8, 3, [0 0 1], [0 0 1]}, 'Cryptobranchoidea' % Caudata
+    {'o', 8, 3, [0 0 1], [0 1 1]}, 'Salamandroidea'
+    {'o', 8, 3, [0 0 1], [1 1 1]}, 'Sirenoidea'
     {'o', 8, 3, [1 0 0], [0 0 0]}, 'Archaeobatrachia' % Anura
-    {'o', 8, 3, [1 0 0], [0 0 1]}, 'Mesobatrachia'
-    {'o', 8, 3, [1 0 0], [1 0 0]}, 'Neobatrachia'
+    {'o', 8, 3, [1 0 0], [1 0 0]}, 'Mesobatrachia'
+    {'o', 8, 3, [1 0 0], [1 1 1]}, 'Neobatrachia'
   };
 end
 
