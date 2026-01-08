a2b5549faeb32dfe23eb1dde3fba4e8dea0bc7b6
Bas Kooijman <bas.kooijman@vu.nl>
2026-01-08 08:22:45 +0100

    extension

--- legend_aves.m
+++ legend_aves.m
@@ -18,18 +18,18 @@ function legend = legend_aves
   % type, size, linewidth, edge color and face color of a marker, taxon
   legend = {...
     {'o', 8, 3, [0 0 0], [0 0 0]}, 'Crocodilia'
-    {'o', 8, 3, [0 0 1], [0 0 0]}, 'Paleognathae'
-    {'o', 8, 3, [0 0 1], [0 0 1]}, 'Galloanserae'
+    {'o', 8, 3, [0 0 1], [0 0 1]}, 'Paleognathae'
+    {'o', 8, 3, [0 0 1], [1 1 1]}, 'Galloanserae'
     % Neoaves: edge magenta and red
     {'o', 8, 3, [1 0 1], [0 0 0]}, 'Mirandornithes'
-    {'o', 8, 3, [1 0 1], [0 0 1]}, 'Gruimorphae'
+    {'o', 8, 3, [1 0 1], [0 1 1]}, 'Gruimorphae'
     {'o', 8, 3, [1 0 1], [1 0 1]}, 'Opisthocomiformes'
-    {'o', 8, 3, [1 0 1], [1 0 0]}, 'Strisores'
+    {'o', 8, 3, [1 0 1], [0 1 1]}, 'Strisores'
     {'o', 8, 3, [1 0 1], [1 1 1]}, 'Columbea'
     % Passerea: edge red
     {'o', 8, 3, [1 0 0], [0 0 0]}, 'Elementaves'
-    {'o', 8, 3, [1 0 0], [0 0 1]}, 'Afroaves'
-    {'o', 8, 3, [1 0 0], [1 0 0]}, 'Australaves'
+    {'o', 8, 3, [1 0 0], [1 0 0]}, 'Afroaves'
+    {'o', 8, 3, [1 0 0], [1 1 1]}, 'Australaves'
     %
     {'.', 8, 3, [0 0 0], [0 0 0]}, 'Animalia'
   };
